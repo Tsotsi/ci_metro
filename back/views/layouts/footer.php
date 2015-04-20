58f496b3f298974827effdf32975096319e52a41
Tsotsi <tsotsi@tsotsi.cn>
2015-04-20 10:37:22 +0800

    'remove'

--- back/views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="page">	
-<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-
-</div>
-</div>
-</body>
-</html>
